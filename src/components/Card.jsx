6294e2c1e3775ed06ff0abbe6faab7143190d42d
Arvind kumar <97538697+Arvind693@users.noreply.github.com>
2022-06-08 14:28:10 +0530

    Add files via upload

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../App.css"
+function Card() {
+  return (
+    <div className='card'>
+        <h2>Mobile Operating System</h2>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>iPhone</li>
+            <li>Windows Phone</li>
+        </ul>
+        <h2>Mobile Manufacturers</h2>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Card
